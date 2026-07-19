49abd908dc60b0711925f32bbb534f891e4d72a8
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-19 10:42:19 +0530

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+  );
+
   app.setGlobalPrefix("api");
 
   app.enableCors({ origin: "http://localhost:3000", credentials: true });
