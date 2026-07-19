c838366a34879e2c780460272eda2b60794ec293
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-19 10:41:37 +0530

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { HealthModule } from "./health/health.module.js";
 import { DatabaseModule } from "./database/database.module.js";
 import {ConfigModule} from "@nestjs/config";
 import {RedisModule} from "./redis/redis.module.js";
+import {DeploymentQueueModule} from "./deployment-queue/deployment-queue.module.js";
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import {RedisModule} from "./redis/redis.module.js";
       }),
       DatabaseModule,
       RedisModule,
+      DeploymentQueueModule,
       HealthModule],
 })
 export class AppModule {}
