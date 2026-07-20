81cd973514bce5fb3ccf9713a202f43a1e136f89
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-20 11:32:04 +0530

    Update deployment-queue.module.ts

--- apps/api/src/deployment-queue/deployment-queue.module.ts
+++ apps/api/src/deployment-queue/deployment-queue.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
-import { DeploymentQueueController } from "./deployment-queue.controller.js";
 import { DeploymentQueueService } from "./deployment-queue.service.js";
 
 @Module({
-    controllers: [DeploymentQueueController],
     providers: [DeploymentQueueService],
     exports: [DeploymentQueueService],
 })
