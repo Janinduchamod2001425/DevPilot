2d8714dbedd28015e93b672a6c78bcc074752a12
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-20 11:33:45 +0530

    Create repository.module.ts

---
+++ apps/worker/src/repository/repository.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { RepositoryService } from "./repository.service.js";
+
+@Module({
+  providers: [RepositoryService],
+  exports: [RepositoryService],
+})
+export class RepositoryModule {}
