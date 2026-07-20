1dcc85c3daed0d24f348dd4a4eb600a2b8dcc0ae
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-20 19:53:40 +0530

    Update deployment-worker.module.ts

--- apps/worker/src/deployment/deployment-worker.module.ts
+++ apps/worker/src/deployment/deployment-worker.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { DeploymentProcessorService } from "./deployment-processor.service.js";
 import { RepositoryModule } from "../repository/repository.module.js";
+import { AnalyzerModule } from "../analyzer/analyzer.module.js";
 
 @Module({
-  imports: [RepositoryModule],
+  imports: [RepositoryModule, AnalyzerModule],
   providers: [DeploymentProcessorService],
 })
 export class DeploymentWorkerModule {}
