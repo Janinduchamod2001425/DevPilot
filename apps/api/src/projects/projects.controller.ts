4b9e2a4d38592887c85fec84fe649c991e618e08
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-22 09:47:03 +0530

    Update projects.controller.ts

--- apps/api/src/projects/projects.controller.ts
+++ apps/api/src/projects/projects.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Param, Patch } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch } from "@nestjs/common";
 import { UpdateRootDirectoryDto } from "./dto/update-root-directory.dto.js";
 import { ProjectsService } from "./projects.service.js";
 
 @Controller("projects")
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get(":projectId/deployments")
+  findDeployments(@Param("projectId") projectId: string) {
+    return this.projectsService.findDeployments(projectId);
+  }
+
+  @Get(":projectId")
+  findOne(@Param("projectId") projectId: string) {
+    return this.projectsService.findOne(projectId);
+  }
 
   @Patch(":projectId/root-directory")
   updateRootDirectory(
