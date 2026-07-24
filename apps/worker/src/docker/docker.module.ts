1bc99f8015ed482d1cc3845e61903e8824f9ac03
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-24 18:01:40 +0530

    Update docker.module.ts

--- apps/worker/src/docker/docker.module.ts
+++ apps/worker/src/docker/docker.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { DockerfileService } from "./dockerfile.service.js";
 import { DockerContainerService } from "./docker-container.service.js";
 import { DockerBuildService } from "./docker-build.service.js";
+import { DatabaseModule } from "../database/database.module.js";
 
 @Module({
+  imports: [DatabaseModule],
   providers: [DockerfileService, DockerBuildService, DockerContainerService],
   exports: [DockerfileService, DockerBuildService, DockerContainerService],
 })
