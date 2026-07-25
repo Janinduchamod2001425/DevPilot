1e62417aeb0c0a462ca86b53101e59147e15e3d8
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-25 11:23:51 +0530

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from "@nestjs/config";
 import { RedisModule } from "./redis/redis.module.js";
 import { DeploymentsModule } from "./deployments/deployments.module.js";
 import { ProjectsModule } from "./projects/projects.module.js";
+import { AuthModule } from "./auth/auth.module.js";
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ProjectsModule } from "./projects/projects.module.js";
     }),
     DatabaseModule,
     RedisModule,
+    AuthModule,
     DeploymentsModule,
     HealthModule,
     ProjectsModule,
