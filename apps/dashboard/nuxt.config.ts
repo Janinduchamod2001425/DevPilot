260985c277f08f6b7933c50a7b63d068c3ab5a0d
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-25 11:24:36 +0530

    Update nuxt.config.ts

--- apps/dashboard/nuxt.config.ts
+++ apps/dashboard/nuxt.config.ts
@@ -2,9 +2,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2026-07-17",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+  devServer: {
+    port: 3001,
+  },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000/api",
+      apiBaseUrl:
+        process.env.NUXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000/api",
     },
   },
   typescript: {
