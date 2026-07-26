d828b1c28a07d3fa3675dd30f5537b0daac617de
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-26 19:52:00 +0530

    Update deployments.module.ts

--- apps/api/src/deployments/deployments.module.ts
+++ apps/api/src/deployments/deployments.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
 import { DeploymentQueueModule } from "../deployment-queue/deployment-queue.module.js";
 import { DeploymentsController } from "./deployments.controller.js";
 import { DeploymentsService } from "./deployments.service.js";
 
 @Module({
-    imports: [DeploymentQueueModule],
-    controllers: [DeploymentsController],
-    providers: [DeploymentsService],
+  imports: [AuthModule, DeploymentQueueModule],
+  controllers: [DeploymentsController],
+  providers: [DeploymentsService],
+  exports: [DeploymentsService],
 })
 export class DeploymentsModule {}
