18d4487c8bbb220b5161c016601be9e73b90418c
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-26 11:44:16 +0530

    Update projects.module.ts

--- apps/api/src/projects/projects.module.ts
+++ apps/api/src/projects/projects.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module.js";
 import { DatabaseModule } from "../database/database.module.js";
+import { GitHubModule } from "../github/github.module.js";
 import { ProjectsController } from "./projects.controller.js";
 import { ProjectsService } from "./projects.service.js";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, GitHubModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
+  exports: [ProjectsService],
 })
 export class ProjectsModule {}
