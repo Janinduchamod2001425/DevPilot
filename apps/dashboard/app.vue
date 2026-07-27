466feccf48e67099bf64627a0f0af16766395146
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-27 19:14:23 +0530

    Update app.vue

--- apps/dashboard/app.vue
+++ apps/dashboard/app.vue
@@ -1,7 +1,24 @@
+<script lang="ts" setup>
+import { Toaster } from "vue-sonner";
+import "vue-sonner/style.css";
+</script>
+
 <template>
-  <div class="min-h-screen bg-slate-950 text-white">
+  <div class="flex min-h-screen flex-col bg-slate-950 text-white">
     <AppHeader />
 
-    <NuxtPage />
+    <main class="flex-1 pt-[72px]">
+      <NuxtPage />
+    </main>
+
+    <ClientOnly>
+      <Toaster
+        :close-button="true"
+        :duration="4000"
+        :rich-colors="true"
+        position="top-right"
+        theme="dark"
+      />
+    </ClientOnly>
   </div>
 </template>
