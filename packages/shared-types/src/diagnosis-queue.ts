a86a3dd0d435b90eee70a1b14e4fea74b2f78cea
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-03 13:06:49 +0530

    Create diagnosis-queue.ts

---
+++ packages/shared-types/src/diagnosis-queue.ts
@@ -0,0 +1,6 @@
+export const DIAGNOSIS_QUEUE_NAME = "diagnosis";
+export const AUTOMATIC_DIAGNOSIS_JOB_NAME = "automatic-diagnosis";
+
+export interface AutomaticDiagnosisJobData {
+  deploymentId: string;
+}
